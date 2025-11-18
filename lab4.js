07fe004dec8b6bd0aa579a7d02e81e26d8b3880b
nirzordas <nirzordas12@gmail.com>
2025-11-18 10:41:34 +0600

    done

---
+++ lab4.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the element where you want to display the message
+    const messageContainer = document.getElementById('messageContainer');
+ 
+    // Insert the message into the container
+    messageContainer.textContent = "Hello, World!";
+});
+ 
