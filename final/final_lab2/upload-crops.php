8346d20996134959470b7a87a82f11f8618acf4e
nirzordas <nirzordas12@gmail.com>
2025-12-23 11:59:11 +0600

    push folder

--- final/final_lab2/upload-crops.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<h2>Crop Uploaded Successfully</h2>";
-echo "Crop: ".$_POST['crop']."<br>";
-echo "Price: ".$_POST['price']."<br>";
-echo "Quantity: ".$_POST['quantity']."<br>";
-echo "<br><a href='dashboard.html'>Back to Dashboard</a>";
-?>
